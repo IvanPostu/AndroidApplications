f24e87661087cdcde027e4ac7caa18274527f254
ivan <ipostu20000127@gmail.com>
2024-06-08 13:06:29 +0300

    Perform refactoring

--- TheNotes/app/src/main/java/com/ipostu/thenotes/MainActivity.kt
+++ TheNotes/app/src/main/java/com/ipostu/thenotes/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.ipostu.thenotes
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
     }
 }
