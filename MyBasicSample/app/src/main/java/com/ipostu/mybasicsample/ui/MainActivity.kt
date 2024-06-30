63390a0ca8c4fa3af07ae0bba5059012fbc99416
ivan <ipostu20000127@gmail.com>
2024-06-30 18:27:14 +0300

    Create an empty project

---
+++ MyBasicSample/app/src/main/java/com/ipostu/mybasicsample/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ipostu.mybasicsample.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ipostu.mybasicsample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        getSupportFragmentManager().beginTransaction()
+//            .add(R.id.fragment_container, fragment, ProductListFragment.TAG).commit();
+    }
+}
